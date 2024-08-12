446c74773c52394a5356a1b8c1eba5f21435f4d0
Karim Elgammal <egkarim@gmail.com>
2024-08-12 23:52:02 +0200

    successfull classical and hybrid working but not finished2

--- quantum_calculations/max_paper/qiskit1_cp2k_2024.1_modified_adapter/Al111/hybrid_calculation/run_experiment_cpu.sh
+++ quantum_calculations/max_paper/qiskit1_cp2k_2024.1_modified_adapter/Al111/hybrid_calculation/run_experiment_cpu.sh
@@ -3,18 +3,18 @@
 set -e
 export OMP_NUM_THREADS=2
 export CP2K_DATA_DIR=/opt/cp2k/data
-rm *.log *.pdos *.cube || true
+rm *.log *.pdos *.cube embedding_socket || true
 echo "Starting CP2K..."
 #mpiexec -np 8 cp2k -i MgO.inp > cp2k_output.log 2>&1 &
 #mpiexec -np 18 cp2k -i supp.inp > cp2k_output.log 2>&1 &
-mpiexec -np 8 cp2k -i Al111.inp > cp2k_output.log 2>&1 &
+mpiexec -np 12 cp2k -i Al111.inp > cp2k_output.log 2>&1 &
 CP2K_PID=$!
 
 echo "Waiting for CP2K to initialize..."
 sleep 30
 
 echo "Starting Python client..."
-python3 -u client-vqe-ucc.py --nalpha 1 --nbeta 1 --norbs 5 2>&1 | tee python_output.log
+python3 -u client-vqe-ucc.py --nalpha 3 --nbeta 3 --norbs 5 2>&1 | tee python_output.log
 
 echo "Waiting for CP2K to finish..."
 wait $CP2K_PID
