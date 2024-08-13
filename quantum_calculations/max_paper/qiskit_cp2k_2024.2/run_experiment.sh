b46088ab047537e5b5b30e8d9ce689d1216330d9
Karim Elgammal <egkarim@gmail.com>
2024-08-14 00:23:51 +0200

    updates with dockerfile for 2024.2

--- quantum_calculations/max_paper/qiskit_cp2k_2024.2/run_experiment.sh
+++ quantum_calculations/max_paper/qiskit_cp2k_2024.2/run_experiment.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
-export OMP_NUM_THREADS=2
+
+# Set the number of MPI processes and OpenMP threads
+NUM_MPI_PROCESSES= 4 #6
+export OMP_NUM_THREADS= 2 #16
+
+# Ensure CP2K can find its data files
 export CP2K_DATA_DIR=/opt/cp2k/data
+
+# Clean up any existing log files
 rm *.log || true
+
 echo "Starting CP2K..."
-#mpiexec -np 8 cp2k -i MgO.inp > cp2k_output.log 2>&1 &
-mpiexec -np 18 cp2k -i supp.inp > cp2k_output.log 2>&1 &
+mpirun -np ${NUM_MPI_PROCESSES} cp2k -i MgO.inp > cp2k_output.log 2>&1 &
 CP2K_PID=$!
 
 echo "Waiting for CP2K to initialize..."
